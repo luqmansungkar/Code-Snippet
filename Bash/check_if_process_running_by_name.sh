99ab61808510f69b8de035bfb7c98ba63f0219d5
Luqman Sungkar <loekmansungkar@gmail.com>
2015-05-31 16:48:59 +0700

    db_php and bash

---
+++ Bash/check_if_process_running_by_name.sh
@@ -0,0 +1,6 @@
+if [ "$(pidof process_name)" ] 
+then
+  # process was found
+else
+  # process not found
+fi
---
+++ Generic PHP/database_related.php
@@ -0,0 +1,44 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "gateway";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if (!$conn) {
+		die("Connection failed: ".mysqli_connect_error());
+	}
+	
+	function dbInsert($table_name, $form_data){
+		//credit : http://www.evoluted.net/thinktank/web-development/time-saving-database-functions
+
+		// retrieve the keys of the array (column titles)
+	    $fields = array_keys($form_data);
+
+	    // build the query
+	    $sql = "INSERT INTO ".$table_name."
+	    (`".implode('`,`', $fields)."`)
+	    VALUES('".implode("','", $form_data)."')";
+	    //echo "sql: ".$sql;
+
+	    return dbQuery($sql);
+	}
+
+	function dbSelect($table_name, $select_query, $where_query){
+		
+		$sql = "SELECT ".$select_query." FROM ".$table_name;
+		if ($where_query != '') {
+			$sql .= " WHERE ".$where_query;
+		}
+		//echo $sql;
+		return dbQuery($sql);
+		
+	}
+
+	function dbQuery($sql){
+		$result = mysqli_query($conn, $sql);
+	    mysqli_close($conn);
+	    return $result;
+	}
+?>
