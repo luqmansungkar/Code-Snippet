34ba48a7aa513779461d03e7c55b1a7538bc4d71
Luqman Sungkar <loekmansungkar@gmail.com>
2015-06-27 00:21:24 +0700

    edit curl

--- Generic PHP/curl.php
+++ Generic PHP/curl.php
@@ -3,12 +3,23 @@
 function kurl($url, $method, $json){
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($json)));
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+		'Content-Length: ' . strlen($json),
+		'Authorization: Basic ZGVsaWdodDpkZWxpZ2h0',
+		));
+
 	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-	curl_setopt($curl, CURLOPT_POSTFIELDS,$json);
+	if ($method != 'GET') {
+		curl_setopt($curl, CURLOPT_POSTFIELDS,$json);
+	}
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 	$result = curl_exec($curl);
+	if($result === false)
+	{
+	    echo 'Curl error: ' . curl_error($curl).'<br>';
+	}
+	curl_close($curl);
 	return $result;
 }
 
